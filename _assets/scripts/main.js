5180410ade7c7628cc0474dfb4987abe3be2e41a
sloveland <samurai@samuelloveland.com>
2013-09-07 05:51:53 -0600

    intro music and animation

--- _assets/scripts/main.js
+++ _assets/scripts/main.js
@@ -111,23 +111,33 @@
 ********************************************** */
 
 var canvas = document.getElementById("easel"),
-	stage = new createjs.Stage(canvas),
-	centerX = canvas.width/2,
-	centerY = canvas.height/2;
-	
-	var logo = new createjs.Bitmap("_assets/images/pokemon_logo.svg");
-	logo.x = centerX;
-	logo.y = centerY;
-	logo.regX = 320;
-	logo.regY = 280;
-	
-	stage.addChild(logo);
+stage = new createjs.Stage(canvas),
+centerX = canvas.width/2,
+centerY = canvas.height/2;
 
+var logo = new createjs.Bitmap("_assets/images/pokemon_logo.png");
+logo.x = centerX;
+logo.y = centerY;
+logo.regX = 320;
+logo.regY = 280;
+logo.alpha = 0.001;
+
+createjs.Sound.registerSound("_assets/music/intro.mp3", "intro");
+createjs.Sound.addEventListener("loadComplete", soundLoaded);
+
+stage.addChild(logo);
+var logoFade = false;
+
+
+
+function soundLoaded(event) {
+	createjs.Sound.play("intro");
+	createjs.Tween.get(logo).wait(2000).to({alpha:1}, 5000);
 	createjs.Ticker.setFPS(30);
 	createjs.Ticker.addListener(function() {
-		// logo.rotation += 5;
 		stage.update();
 	});
+}
 
 /* **********************************************
      Begin controller.js
