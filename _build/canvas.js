de02ea483479d9ee6707d47fe5c4f2ed8bd54220
sloveland <samurai@samuelloveland.com>
2013-09-07 17:25:11 -0600

    game controls

--- _build/canvas.js
+++ _build/canvas.js
@@ -1,31 +1,36 @@
-var canvas = document.getElementById("easel"),
-stage = new createjs.Stage(canvas),
-centerX = canvas.width/2,
-centerY = canvas.height/2;
+function initCanvas() {
+	console.log("initCanvas() called");
+	var canvas = document.getElementById("easel"),
+	stage = new createjs.Stage(canvas),
+	centerX = canvas.width/2,
+	centerY = canvas.height/2;
 
-var background = new createjs.Shape();
-background.graphics.beginFill("#000000").drawRect(0,0, canvas.width, canvas.height)
+	var background = new createjs.Shape();
+	background.graphics.beginFill("#000000").drawRect(0,0, canvas.width, canvas.height);
 
-var logo = new createjs.Bitmap("_assets/images/pokemon_logo.png");
-logo.x = centerX;
-logo.y = centerY;
-logo.regX = 320;
-logo.regY = 280;
-logo.alpha = 0.001;
+	var logo = new createjs.Bitmap("_assets/images/pokemon_logo.png");
+	logo.x = centerX;
+	logo.y = centerY;
+	logo.regX = 320;
+	logo.regY = 280;
+	logo.alpha = 0.001;
 
-createjs.Sound.registerSound("_assets/music/intro.mp3", "intro");
-createjs.Sound.addEventListener("loadComplete", soundLoaded);
+	createjs.Sound.registerSound("_assets/music/intro.mp3", "intro");
+	createjs.Sound.addEventListener("loadComplete", soundLoaded);
 
-stage.addChild(background);
-stage.addChild(logo);
-var logoFade = false;
+	stage.addChild(background);
+	stage.addChild(logo);
+	var logoFade = false;
+}
+
+
 
 
 
 function soundLoaded(event) {
 	createjs.Sound.play("intro");
 	createjs.Tween.get(logo).wait(2000).to({alpha:1}, 5000);
-	createjs.Tween.get(background).wait(7000).to({alpha: 0}, 5000)
+	createjs.Tween.get(background).wait(7000).to({alpha: 0}, 5000);
 	createjs.Ticker.setFPS(30);
 	createjs.Ticker.addListener(function() {
 		stage.update();
--- _build/controller.js
+++ _build/controller.js
@@ -1,10 +1,90 @@
 /*jslint browser:true */
 
-var charmander1 = new Pokemon("Charmander", 5, 20, 11, 10, 13);
-var bulbasaur1 = new Pokemon("Bulbasaur", 5, 21, 11, 11, 11);
+// Game Namespace
+var Game = Game || {};
+Game.screen = Game.screen || {};
+Game.controls = Game.controls || {};
+Game.scene = Game.scene || {};
 
-document.onload = function () {
-	initCanvas();
+// Game Controls
+Game.controls.choose = function() {
+	console.log("Choose");
+}
+Game.controls.cancel = function() {
+	console.log("Cancel");
+}
+Game.controls.start = function() {
+	console.log("Start");
+}
+Game.controls.select = function() {
+	console.log("Select");
+}
+Game.controls.up = function() {
+	console.log("Up");
+}
+Game.controls.right = function() {
+	console.log("Right");
+}
+Game.controls.down = function() {
+	console.log("Down");
+}
+Game.controls.left = function() {
+	console.log("Left");
+}
+
+
+document.body.onload = function () {
+	console.log(Game);
 };
 
 
+window.addEventListener('keydown', function(event) {
+	switch (event.keyCode) {
+		// Choose
+		case 13: // Enter
+		case 32: // Spacebar
+			Game.controls.choose();
+		break;
+
+		// Cancel
+		case 8: // Backspace
+		case 16: // Shift
+			Game.controls.cancel();
+		break;
+
+		// Start
+		case 17: // Ctrl
+		case 36: // Home
+			Game.controls.start();
+		break;
+
+		// Shift
+		case 9: // Tab
+		case 18: // Alt
+			Game.controls.select();
+		break;
+
+		// Up
+		case 38: // Up Arrow
+		case 87: // W
+			Game.controls.up();
+		break;
+
+		// Right
+		case 39: // Right Arrow
+		case 68: // D
+			Game.controls.right();
+		break;
+
+		// Down
+		case 40: // Down Arrow
+		case 83: // S
+			Game.controls.down();
+		break;
+
+		// Left
+		case 37: // Left Arrow
+		case 65: // A
+			Game.controls.left();
+	}
+}, false);
